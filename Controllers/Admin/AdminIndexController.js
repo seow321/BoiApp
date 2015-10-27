98b4abecd7a1a8c97d43aae89f93a750eb75dd28
YuggoHor <yuggohor91@gmail.com>
2015-10-27 23:37:46 +0800

    dealer list

--- Controllers/Admin/AdminIndexController.js
+++ Controllers/Admin/AdminIndexController.js
@@ -1,6 +1,20 @@
-﻿boiApp.controller('AdminIndexController', function ($rootScope, $scope, $http, $timeout) {
+﻿
+
+var boiApp = angular.module('boiApp');
+
+boiApp.controller('AdminIndexController', function ($rootScope, $scope, $http, $timeout) {
     $scope.$on('$viewContentLoaded', function () {
         // initialize core components
-
+        
+        
     });
+
+    $scope.person = [
+        { name: "Kek Sim" }, { name: "Xiang" },
+        { name: "Kek Sim" }, { name: "Xiang" },
+        { name: "Kek Sim" }, { name: "Xiang" },
+        { name: "Kek Sim" }, { name: "Xiang" },
+        { name: "Kek Sim" }, { name: "Xiang" },
+        { name: "Kek Sim" }, { name: "Xiang" }
+    ];
 });
