7e97f350576768a31574518ca37c50bfd0051c8f
Victor Seow <seow321@gmail.com>
2015-10-29 00:07:11 +0800

    Added Simple notification
    Simple Notification can be used now

---
+++ Controllers/NotificationController.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+var boiApp = angular.module('boiApp');
+
+boiApp.controller('NotificationController', function ($rootScope, $scope, $http, $timeout) {
+    $scope.$on('$viewContentLoaded', function () {
+        // initialize core components
+        
+    });
+
+    $scope.notificationList = [
+        { title: "New Deal", details: "Agent 3344 made a deal with Client 5566." },
+        { title: "New Deal", details: "Agent 3344 made a deal with Client 5566." },
+        { title: "New Deal", details: "Agent 3344 made a deal with Client 5566." },
+        { title: "New Deal", details: "Agent 3344 made a deal with Client 5566." },
+        { title: "New Deal", details: "Agent 3344 made a deal with Client 5566." },
+    ];
+});
