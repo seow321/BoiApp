5e4c2980fd00d52494ce75aefc8ad2845f18ecce
Victor Seow <seow321@gmail.com>
2015-10-31 14:56:34 +0800

    Reworked v1.2

---
+++ Controllers/Dealer/DealerController.js
@@ -0,0 +1,26 @@
+﻿
+
+var boiApp = angular.module('boiApp');
+
+boiApp.controller('AdminIndexController', function ($rootScope, $scope, $http, $timeout) {
+    $scope.$on('$viewContentLoaded', function () {
+        // initialize core components
+        
+        
+    });
+
+    $scope.person = [
+        { name: "Kek Sim" }, { name: "Xiang" },
+        { name: "Kek Sim" }, { name: "Xiang" },
+        { name: "Kek Sim" }, { name: "Xiang" },
+        { name: "Kek Sim" }, { name: "Xiang" },
+        { name: "Kek Sim" }, { name: "Xiang" },
+        { name: "Kek Sim" }, { name: "Xiang" }
+    ];
+
+    $scope.selected = null;
+
+    $scope.select = function (person) {
+        $scope.selected = person;
+    }
+});
