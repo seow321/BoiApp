5b4644e70de0359fc944ed906448fbfe5e99d7a8
YuggoHor <yuggohor91@gmail.com>
2015-11-05 00:17:59 +0800

    v2.2 Reworked Dealer
    -added fade effect.
    -added profile details.

--- Controllers/Admin/AdminIndexController.js
+++ Controllers/Admin/AdminIndexController.js
@@ -5,22 +5,21 @@ var boiApp = angular.module('boiApp');
 boiApp.controller('AdminIndexController', function ($rootScope, $scope, $http, $timeout) {
     $scope.$on('$viewContentLoaded', function () {
         // initialize core components
-        
+
+        $http.get('Views/Admin/dealer.json')
+        .success(function (data) {
+            $scope.dealers = data;
+        });
         
     });
 
-    $scope.person = [
-        { name: "Kek Sim" }, { name: "Xiang" },
-        { name: "Kek Sim" }, { name: "Xiang" },
-        { name: "Kek Sim" }, { name: "Xiang" },
-        { name: "Kek Sim" }, { name: "Xiang" },
-        { name: "Kek Sim" }, { name: "Xiang" },
-        { name: "Kek Sim" }, { name: "Xiang" }
-    ];
+    $scope.dealers = [];
+
+    $scope.search = false;
 
     $scope.selected = null;
 
-    $scope.select = function (person) {
-        $scope.selected = person;
+    $scope.select = function (dealer) {
+        $scope.selected = dealer;
     }
 });
